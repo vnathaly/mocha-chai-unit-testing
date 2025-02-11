d33ddd3de828be4ec4333b0cc518c5e53a65f644
Intellisys <intellisys@MacBook-Pro-de-Intellisys.local>
2025-02-11 09:21:59 -0400

    Delete redundancy

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,26 +6,18 @@ describe('Test Calculator Class', ()=>{
   const cal = new Calculator();
 
   it('Should return sum',()=>{
-    //arrange
-    // const cal = new Calculator();
     const result = cal.add(2,3);
     expect(result).to.equal(5);
   })
   it('Should return substraction', ()=>{
-    // //arramge
-    // const cal = new Calculator();
     const result = cal.substract(4,1);
     expect(result).to.equal(3);
   })
   it('Should return Multiply', ()=>{
-    // //arrange
-    // const cal = new Calculator();
     const result = cal.multiply(1,2);
     expect(result).to.equal(2);
   })
   it('Should return divide', ()=>{
-    // //arrange
-    // const cal = new Calculator();
     const result = cal.divide(15,3);
     expect(result).to.equal(5);
   })
