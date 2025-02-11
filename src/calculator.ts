9359f48f7316a26c25196dea3ec484395f8cb056
Intellisys <intellisys@MacBook-Pro-de-Intellisys.local>
2025-02-11 09:19:23 -0400

    Tests running

---
+++ src/calculator.ts
@@ -0,0 +1,14 @@
+export default class Calculator {
+    add(a:number, b:number):number {
+        return a+b;
+    }
+    substract(a:number, b:number):number {
+        return a-b;
+    }
+    multiply(a:number, b:number):number {
+        return a*b;
+    }
+    divide(a:number, b:number):number {
+        return a/b;
+    }
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,32 @@
 import "mocha"
 import {expect} from "chai"
+import Calculator from "../src/calculator"
 
-describe('test suite', () => {
- 
-  it('test case', function() {
-    const num : number = 5
-    expect(num).to.be.above(4)
-   })
+describe('Test Calculator Class', ()=>{
+  const cal = new Calculator();
 
+  it('Should return sum',()=>{
+    //arrange
+    // const cal = new Calculator();
+    const result = cal.add(2,3);
+    expect(result).to.equal(5);
+  })
+  it('Should return substraction', ()=>{
+    // //arramge
+    // const cal = new Calculator();
+    const result = cal.substract(4,1);
+    expect(result).to.equal(3);
+  })
+  it('Should return Multiply', ()=>{
+    // //arrange
+    // const cal = new Calculator();
+    const result = cal.multiply(1,2);
+    expect(result).to.equal(2);
+  })
+  it('Should return divide', ()=>{
+    // //arrange
+    // const cal = new Calculator();
+    const result = cal.divide(15,3);
+    expect(result).to.equal(5);
+  })
 })
